e086957570ee6b180e2499d332c03a41d3a3bcd0
taehan79-kim <84124094+taehan79-kim@users.noreply.github.com>
2023-11-14 15:02:13 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 92 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -0,0 +1,19 @@
+a = int(input())
+for i in range(1, a*2):
+    c=""
+    if i <= a:
+        for b in range(1, a * 2):
+            if b < a + i:
+                if b > a - i:
+                    c = c + '*'
+                else:
+                    c = c + ' '
+        print(c)
+    else:
+        for b in range(1, a*2):
+            if b < a+(a-(i-a)):
+                if b > a-(a-(i-a)):
+                    c = c + '*'
+                else:
+                    c = c + ' '
+        print(c)
