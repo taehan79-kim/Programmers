8fadbd2f816abbeac49f6426ddd4a0f0f2ec5bed
taehan79-kim <84124094+taehan79-kim@users.noreply.github.com>
2023-11-14 15:04:46 +0900

    [Bronze II] Title: 숫자의 개수, Time: 68 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -0,0 +1,14 @@
+x = []
+for i in range(3):
+    x.append(int(input()))
+cnt = str(x[0] *x[1]*x[2])
+print(cnt.count('0'))
+print(cnt.count('1'))
+print(cnt.count('2'))
+print(cnt.count('3'))
+print(cnt.count('4'))
+print(cnt.count('5'))
+print(cnt.count('6'))
+print(cnt.count('7'))
+print(cnt.count('8'))
+print(cnt.count('9'))
