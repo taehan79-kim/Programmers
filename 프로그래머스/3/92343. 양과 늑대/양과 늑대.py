65057ce5842eac2c05982874b5265a1c009a92e3
taehan79-kim <84124094+taehan79-kim@users.noreply.github.com>
2023-11-22 16:37:42 +0900

    [level 3] Title: 양과 늑대, Time: 0.74 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -0,0 +1,30 @@
+def dfs(sheep, wolf, edges):
+    if sheep > wolf:
+        a.append(sheep)
+    else:
+        return 
+        
+    for p, c in edges:
+        if visited[p] and not visited[c]:
+            visited[c] = 1
+            if _info[c] == 0:
+                dfs(sheep+1, wolf, edges)
+            else:
+                dfs(sheep, wolf+1, edges)
+            visited[c] = 0
+
+def solution(info, edges):
+    global visited
+    visited = [0] * len(info)
+    global _info
+    global a
+    answer = []
+    a =[]
+    
+    _info = info
+        
+	# 루트 노드부터 시작
+    visited[0] =1
+    dfs(1,0, edges)
+    print(a)
+    return max(a)
