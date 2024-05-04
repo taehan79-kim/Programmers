d3eb5929f41e806a5a0bc615053ef91917bb7368
taehan79-kim <84124094+taehan79-kim@users.noreply.github.com>
2024-05-04 21:36:27 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4156 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    queue<int> q;
+    int N;
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        q.push(i);
+    }
+    while (q.size() > 1) {
+        q.pop();
+        q.push(q.front());
+        q.pop();
+    }
+    cout << q.front();
+}
