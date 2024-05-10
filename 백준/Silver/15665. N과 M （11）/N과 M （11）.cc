1ab46be9d190b0d5e4c2b634fc572b10168a6db5
taehan79-kim <84124094+taehan79-kim@users.noreply.github.com>
2024-05-10 22:00:50 +0900

    [Silver II] Title: N과 M (11), Time: 124 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define X first
+#define Y second
+
+int N, M;
+int arr[10];
+int num[10];
+int cnt = 0;
+bool isused[10];
+
+void func1(int cur) {
+    if (cur == M) {
+        for (int i = 0; i < M; i++) {
+            cout << num[arr[i]] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    int tmp = 0;
+    int st = 0;
+    if (cur > 0) st = arr[cur-1];
+    for (int i = 0; i < N; i++) {
+        if (num[i] != tmp) {
+            arr[cur] =i;
+            isused[i] = true;
+            tmp = num[arr[cur]];
+            func1(cur + 1);
+            isused[i] = false;
+        }
+    }
+}
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> M;
+    for (int i = 0; i < N; i++)cin >> num[i];
+    sort(num, num + N);
+    func1(0);
+}
