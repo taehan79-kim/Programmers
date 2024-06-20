d9882017e4a5586288773b037a3c6ec85ab59c10
taehan79-kim <84124094+taehan79-kim@users.noreply.github.com>
2024-06-20 17:43:49 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int d[1005];
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    d[1] = 1;
+    d[2] = 3;
+
+    for (int i = 3; i <= n; i++) {
+        d[i] = d[i - 1] + 2 * d[i - 2];
+        d[i] = d[i] % 10007;
+    }
+    cout << d[n];
+}
