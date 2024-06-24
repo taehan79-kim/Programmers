22e0b6ab224d2fb520a9f61bf30ed7fc6074238c
taehan79-kim <84124094+taehan79-kim@users.noreply.github.com>
2024-06-24 18:31:10 +0900

    [Gold V] Title: 퇴사 2, Time: 220 ms, Memory: 25460 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int t[1500002], p[1500002];
+long long d[1500002];
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        cin >> t[i] >> p[i];
+    }
+    for (int i = n; i >= 1; i--) {
+        if (i + t[i] <= n + 1) { // 일을 할 수 있는 경우
+            d[i] = max(d[i + t[i]] + p[i], d[i + 1]);
+        }
+        else d[i] = d[i + 1]; // 일을 못하는 경우
+    }
+    cout << *max_element(d, d + n + 1);
+}
