84b409bd896b497f0c7037264cd3a4c73c8cab95
taehan79-kim <84124094+taehan79-kim@users.noreply.github.com>
2024-06-24 22:53:59 +0900

    [Gold V] Title: 동전, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m;
+int a[10005], d[10005];
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        fill(d, d + 10005, 0);
+        cin >> n;
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+        cin >> m;
+        d[0] = 1;
+        for (int i = 0; i < n; i++)
+            for (int j = a[i]; j <= m; j++)
+                d[j] += d[j - a[i]]; // j - a[i]에 a[i] 동전 추가
+        cout << d[m] << "\n";
+    }
+}
