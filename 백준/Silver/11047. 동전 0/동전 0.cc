18486eb9ef1620b68073f544d7bece0a3e4d804b
taehan79-kim <84124094+taehan79-kim@users.noreply.github.com>
2024-06-25 14:07:17 +0900

    [Silver IV] Title: 동전 0, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n,k;
+int a[15];
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int ans = 0;
+    cin >> n >> k;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = n-1; i >= 0; i--) {
+        if (int(k / a[i]) > 0) {
+            ans += int(k / a[i]);
+            k = k % a[i];
+        }
+    }
+    cout << ans;
+}
