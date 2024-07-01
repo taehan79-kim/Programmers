93cf7227e940891a968404dbc8181556207e7a2d
taehan79-kim <84124094+taehan79-kim@users.noreply.github.com>
2024-07-01 16:20:01 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    long a;
+    cin >> a;
+    cout << a * 4;
+    return 0;
+}
